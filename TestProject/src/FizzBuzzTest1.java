5b6d6509af14994f51238c8e740502c46744e750
Lenovo <Lenovo@DESKTOP-P2SHG6E>
2020-11-22 15:41:45 -0800

    First Commit

--- TestProject/src/FizzBuzzTest1.java
+++ TestProject/src/FizzBuzzTest1.java
@@ -1,12 +1,17 @@
 
 public class FizzBuzzTest1 {
 	
-	    public static void main(String args[]){
+	    public static void main(String args[])
+	    {
 	    
-	        for(int i = 1; i <= 100; i++) {
-	            if(i % (3*5) == 0) System.out.println("FizzBuzz");
-	            else if(i % 5 == 0) System.out.println("Buzz");
-	            else if(i % 3 == 0) System.out.println("Fizz");
+	        for(int i = 1; i <= 100; i++) 
+	        {
+	            if(i % 3 == 0)    //for multiples of 3 print “Fizz”
+	        		System.out.println("Fizz");
+	        	else if(i % 5 == 0)   //for multiples of 5 print “Buzz”
+		            	System.out.println("Buzz");
+	            if(i % (3*5) == 0)    //for multiples of 3 and 5 print “FizzBuzz”
+	            	System.out.println("FizzBuzz");
 	            else System.out.println(i);
 	        } 
 	    } 
